43b16890505ddf9602760e43f9fcd90d2f569c4c
Jounathan-P <jounathan@stu.usc.edu.tt>
2021-01-29 12:52:13 -0400

    Java Project 1

--- src/Programming_Lab1_Question1.java
+++ src/Programming_Lab1_Question1.java
@@ -5,22 +5,26 @@ public class Programming_Lab1_Question1 {
 
         Scanner sc = new Scanner(System.in);
 
-        int accountNum, numOfMin, timeDuringDay, timeDuringNight;
+        int accountNum, numOfMin = 0, timeDuringDay = 0, timeDuringNight = 0;
         char serviceCode;
-        double amountDue, dayCost, nightCost;
+        double amountDue = 0, dayCost, nightCost;
+        String typeOfService = null;
+        boolean lp = false;
 
         System.out.print("Please Enter Your Account Number: ");
         accountNum = sc.nextInt();
 
-        System.out.print("Please Enter Your Service Code ((A) for Regular Service or (B) for Premium: ");
+        System.out.print("Please Enter Your Service Code ((A) for Regular Service or (B) for Premium): ");
         serviceCode = sc.next().charAt(0);
 
 
-        do {
+        while (lp == false) {
             if (serviceCode == 'A' || serviceCode == 'a') {
                 System.out.print("Please Enter Number of Minutes The Service was Used: ");
                 numOfMin = sc.nextInt();
                 amountDue = 10 + ((numOfMin - 50) * 0.20);
+                typeOfService = "Regular";
+                lp = true;
             } else if (serviceCode == 'B' || serviceCode == 'b') {
                 System.out.print("Please Enter the Amount of Minutes The Service was Used During the Day: ");
                 timeDuringDay = sc.nextInt();
@@ -28,14 +32,18 @@ public class Programming_Lab1_Question1 {
                 System.out.print("Please Enter the Amount of Minutes The Service was Used During the Night: ");
                 timeDuringNight = sc.nextInt();
 
+                numOfMin = timeDuringDay + timeDuringNight;
                 dayCost = ((timeDuringDay - 75) * 0.10);
                 nightCost = ((timeDuringNight - 100) * 0.05);
                 amountDue = 25 + dayCost + nightCost;
+                typeOfService = "Premium";
+                lp = true;
             } else {
                 System.out.print("Please Enter (A) for Regular Service or (B) for Premium: ");
                 serviceCode = sc.next().charAt(0);
+                lp = false;
             }
         }
-        while(serviceCode != 'A' || serviceCode != 'a' || serviceCode != 'B' || serviceCode != 'b');
+        System.out.print("Account Number: " + accountNum + "\nType of Service: " + typeOfService + "\nAmount of Time Used: " + numOfMin + "\nAmount Due: $" + amountDue);
     }
 }
